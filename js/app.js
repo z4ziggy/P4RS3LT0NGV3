41e9173c6dabe75e126876daaa05c13f4736b687
EP <ep@Mac.lan>
2025-08-20 19:48:23 -0700

    Tokenade: enable Auto-copy by default

--- js/app.js
+++ js/app.js
@@ -64,7 +64,7 @@ window.app = new Vue({
         tbIncludeVS: true,
         tbIncludeNoise: true,
         tbRandomizeEmojis: true,
-        tbAutoCopy: false,
+        tbAutoCopy: true,
         tbSingleCarrier: true,
         tbCarrier: '',
         tbPayloadEmojis: [],
